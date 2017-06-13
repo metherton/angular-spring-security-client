03b20852f2a61e8a3b849ad7007ed81b166a194c
metherton <martinetherton63@gmail.com>
2017-06-13 22:51:27 +0200

    reenable unit testst

--- src/test/java/demo/UiApplicationTests.java
+++ src/test/java/demo/UiApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = UiApplication.class)
 @WebAppConfiguration
-@Ignore
 public class UiApplicationTests {
 
 	@Test
